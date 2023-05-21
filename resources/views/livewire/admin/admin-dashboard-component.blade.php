a6cefedd04614b7d1a7e6e10842bda647c166d7c
lara <laravel8coi@gmail.com>
2023-05-21 19:12:06 +0530

    21may20230711

--- resources/views/livewire/admin/admin-dashboard-component.blade.php
+++ resources/views/livewire/admin/admin-dashboard-component.blade.php
@@ -439,7 +439,7 @@
                                           <li><a class="dropdown-item" href="#" wire:click.prevent="updateEventstatus({{$info->id}},'1')">Active</a></li>
                                       @endif
                                       <li><a class="dropdown-item" href="#" onclick="confirm('Are you sure, You want to delete this Entity?') || event.stopImmediatePropagation()"  wire:click.prevent="eventdelete({{$info->id}})"> <i class="bi bi-x me-2"></i> Delete</a></li>
-                                      <li><a class="dropdown-item" href="{{route('admin.eventEdit',['event_id' => $info->id])}}"><i class="bi bi-pencil me-2"></i>Edit</a></li>
+                                      <li><a class="dropdown-item" href="{{route('admin.eventEdit',['event_id' => $evento->id , 'board' => 'edit'])}}"><i class="bi bi-pencil me-2"></i>Edit</a></li>
                                       <li><a class="dropdown-item" href="{{route('adminevent.detail',['slug' => $info->slug])}}"><i class="bi bi-note me-2"></i>Details</a></li>
                                   </ul>
                               </div>        
