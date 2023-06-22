ed9eb23791b63e4f7343d50bb19e62858944299a
lara <laravel8coi@gmail.com>
2023-06-22 15:17:40 +0530

    22june2030317

--- resources/views/livewire/conference-component.blade.php
+++ resources/views/livewire/conference-component.blade.php
@@ -16,7 +16,7 @@
           
       <div class="d-flex badgeseconf">
             @foreach( $finder as $categ) 
-            <a class="badge border-1 text-right border-dark text-dark mr-1" href="{{route('coi.exhibitioncategory',['eventype' => 'conference', 'categry_id' => $categ->expo_id])}}">
+            <a class="badge border-1 text-right border-dark text-dark mr-1" href="{{route('coi.exhibitioncategory',['eventype' => 'conference', 'categry_id' => $categ->id])}}">
               {{ucwords(trans($categ->expo->tag))}}</a>
             @endforeach
       </div>
