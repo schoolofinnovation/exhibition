bb18a8b7353d63437ba883cb848f18eba5b60a62
lara <laravel8coi@gmail.com>
2023-09-16 13:34:49 +0530

    16sept20230134

--- resources/views/livewire/admin/admin-dashboard-component.blade.php
+++ resources/views/livewire/admin/admin-dashboard-component.blade.php
@@ -968,7 +968,7 @@
               <input type="text" class="form-control" placeholder="search with ID" wire:model.lazy="searchBrandTerm">
               
               <div class="row mb-5 pb-2">
-                @if(count($searchBrandTerm) == 0)
+                @if(is_null($searchBrandTerm))
 
                   <div class="container">
                     Find Some Events
@@ -977,7 +977,65 @@
                 @else
 
                   @if($searchBrandcat->count() == 0)
-                    @foreach ($searchBrandcat as $franchiseo) 
+                   
+                    <div class="container">
+                      <div class="fs-md">Update Contact Card</div>
+                      <form wire:submit.prevent="directbrandBcontact">
+                          <input type="text" class="form-control" placeholder="organisation" wire:model.lazy="organisation">
+                          <input type="text" class="form-control" placeholder="brand_name" wire:model.lazy="brand_name">
+                          
+                          <input type="text" class="form-control" placeholder="industry" wire:model.lazy="industry">
+                          
+                          <input type="text" class="form-control" placeholder="name" wire:model.lazy="name">
+                          <input type="text" class="form-control" placeholder="designation" wire:model.lazy="designation">
+                          <input type="number" class="form-control" placeholder="phone" wire:model.lazy="phone">
+                          <input type="email" class="form-control" placeholder="email" wire:model.lazy="email">
+                          
+                          <button class="form-control  btn btn-primary" type="submit">Submit</button>
+                      </form>
+                    </div>
+          
+                    <div class="container my-5">
+                        <div class="fs-md">
+                        Contact details 
+                        </div>
+                            @foreach ($getContact as $franchise) 
+                                <div class="">
+                                    <div class="row text-center p-1 gx-0 mb-1  shadow-sm  border rounded border-1">
+                                        <div class="col  pr-0">
+                                        
+                                            <div class="h4 fw-light mb-0"> 1 </div> 
+                                            <div class="small text-muted">chk </div>
+                                            
+                                            <div class="round-circle" ><i class="bi bi-bookmark"></i></div> 
+                                        </div>
+
+                                        <div class="col-7  p-0">
+                                        <div class="fs-md fw-normal text-start"><a class="text-dark" href="#">
+                                            {{$franchise->name}} {{$franchise->designation}}</a></div>
+                                        <div class="text-muted fs-sm text-start">
+                                            {{$franchise->email}}
+                                        </div>  
+                                        <div class="text-muted fs-sm text-start">{{$franchise->phone}}</div>
+                                        </div>
+
+                                        <div class="col-3  p-0">
+                                            {{--<a class="card-img-top d-block overflow-hidden" href="#">
+                                                <img src="{{url('exhibition/'.$franchise->image)}}" alt="{{Str::limit($franchise->eventname, 24)}}"></a>--}}
+                                                
+                                            {{-- <a class="round-circle" href="{{route('event.details',['slug' => $franchise->slug])}}">
+                                                <i class="bi bi-chevron-double-right"></i></a> 
+                                                <a class="btn btn-primary btn-sm" href="#" wire:click.prevent="claimer({{$franchise->id}})" >Claim</a> --}}
+
+                                                <a class="btn btn-primary btn-sm" href="#" wire:click.prevent="del({{$franchise->id}})">Delete</a>
+
+                                            </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                    </div>
+                  @else
+                  @foreach ($searchBrandcat as $franchiseo) 
                         <div class="">
                         {{$franchiseo -> brand_name}} > {{$franchiseo -> organisation}}
 
@@ -1020,63 +1078,6 @@
                           
                         </div>
                     @endforeach
-                  @else
-                  <div class="container">
-                      <div class="fs-md">Update Contact Card</div>
-                      <form wire:submit.prevent="directbrandBcontact">
-                          <input type="text" class="form-control" placeholder="organisation" wire:model.lazy="organisation">
-                          <input type="text" class="form-control" placeholder="brand_name" wire:model.lazy="brand_name">
-                          
-                          <input type="text" class="form-control" placeholder="industry" wire:model.lazy="industry">
-                          
-                          <input type="text" class="form-control" placeholder="name" wire:model.lazy="name">
-                          <input type="text" class="form-control" placeholder="designation" wire:model.lazy="designation">
-                          <input type="number" class="form-control" placeholder="phone" wire:model.lazy="phone">
-                          <input type="email" class="form-control" placeholder="email" wire:model.lazy="email">
-                          
-                          <button class="form-control  btn btn-primary" type="submit">Submit</button>
-                      </form>
-                  </div>
-        
-                  <div class="container my-5">
-                      <div class="fs-md">
-                      Contact details 
-                      </div>
-                          @foreach ($getContact as $franchise) 
-                              <div class="">
-                                  <div class="row text-center p-1 gx-0 mb-1  shadow-sm  border rounded border-1">
-                                      <div class="col  pr-0">
-                                      
-                                          <div class="h4 fw-light mb-0"> 1 </div> 
-                                          <div class="small text-muted">chk </div>
-                                          
-                                          <div class="round-circle" ><i class="bi bi-bookmark"></i></div> 
-                                      </div>
-
-                                      <div class="col-7  p-0">
-                                      <div class="fs-md fw-normal text-start"><a class="text-dark" href="#">
-                                          {{$franchise->name}} {{$franchise->designation}}</a></div>
-                                      <div class="text-muted fs-sm text-start">
-                                          {{$franchise->email}}
-                                      </div>  
-                                      <div class="text-muted fs-sm text-start">{{$franchise->phone}}</div>
-                                      </div>
-
-                                      <div class="col-3  p-0">
-                                          {{--<a class="card-img-top d-block overflow-hidden" href="#">
-                                              <img src="{{url('exhibition/'.$franchise->image)}}" alt="{{Str::limit($franchise->eventname, 24)}}"></a>--}}
-                                              
-                                          {{-- <a class="round-circle" href="{{route('event.details',['slug' => $franchise->slug])}}">
-                                              <i class="bi bi-chevron-double-right"></i></a> 
-                                              <a class="btn btn-primary btn-sm" href="#" wire:click.prevent="claimer({{$franchise->id}})" >Claim</a> --}}
-
-                                              <a class="btn btn-primary btn-sm" href="#" wire:click.prevent="del({{$franchise->id}})">Delete</a>
-
-                                          </div>
-                                  </div>
-                              </div>
-                          @endforeach
-                  </div>
                   @endif
 
                 @endif
