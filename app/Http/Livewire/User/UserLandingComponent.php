c4ba39985faccfeeb74aa34c38209681a99c2533
lara <laravel8coi@gmail.com>
2023-12-21 21:52:50 +0530

    21dec20230952

--- app/Http/Livewire/User/UserLandingComponent.php
+++ app/Http/Livewire/User/UserLandingComponent.php
@@ -2,25 +2,108 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\bcontact;
 use App\Models\Magazine;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
 use Livewire\Component;
+use Stringable;
 
 class UserLandingComponent extends Component
 {
     public $trackcustomer;
+    public $board;
+
+    public $name;
+    public $slug;
+    public $type;
+    public $subscriber;
+    public $desc;
+    public $frequency;
+    public $status;
+    public $admstatus;
+    public $user_id;
+
+
+    public $released;
+    public $updated;
+    public $version;
+    public $category;
+
+
+    public $phone;
+    public $email;
 
 
     public function mount($trackcustomer)
     {
       $this->trackcustomer = $trackcustomer;
+        $fattribute = Auth::user();
+        $this->name = $fattribute->name;
+        $this->email = $fattribute->email;
+        $this->phone = $fattribute->phone;
     }
+
+
+
+    public function added()
+    {
+       $magazineDetails = new Magazine();
+       $magazineDetails->name = $this->name;
+       $magazineDetails->slug = Str::slug($this->name,'-');
+       $magazineDetails->type = $this->type;
+       $magazineDetails->subscriber = $this->subscriber;
+       $magazineDetails->desc = $this->desc;
+       $magazineDetails->frequency = $this->frequency;
+       $magazineDetails->status = '1';
+       $magazineDetails->admstatus = '1';
+
+       $magazineDetails->user_id = Auth::user()->id;
+       $magazineDetails->save(); 
+
+    }
+
+
+    public function printedfacts()
+    {
+      $magazineDetails = new Magazine();
+       $magazineDetails->released = $this->released;
+       $magazineDetails->updated = $this->updated;
+       $magazineDetails->version = $this->version;
+       $magazineDetails->category = $this->category;
+
+       $magazineDetails->status = '1';
+       $magazineDetails->admstatus = '1';
+
+       $magazineDetails->user_id = Auth::user()->id;
+       $magazineDetails->save();
+    }
+
+    public function updatecontact()
+    {
+       $magazineDetails = new bcontact();
+       $magazineDetails->name = $this->name;
+       $magazineDetails->designation = $this->designation;
+       $magazineDetails->phone = $this->phone;
+       $magazineDetails->email = $this->email;
+       $magazineDetails->status = '1';
+       $magazineDetails->admstatus = '1';
+       $magazineDetails->user_id = Auth::user()->id;
+       $magazineDetails->save();
+    }
+
+
 
 
     public function render()
     {
         $magazine = Magazine::where('user_id', Auth::user()->id)->get();
+
+        $updatephone = Auth::user()->phone;
+        $updateemail = Auth::user()->email;
+        $updatename = Auth::user()->name;
+
         
-        return view('livewire.user.user-landing-component', ['magazine' => $magazine]);
+        return view('livewire.user.user-landing-component', ['updatephone' => $updatephone,'updateemail' => $updateemail,'updatename' => $updatename,'magazine' => $magazine]);
     }
 }
--- resources/views/livewire/user/user-landing-component.blade.php
+++ resources/views/livewire/user/user-landing-component.blade.php
@@ -12,66 +12,92 @@
             </div>
         @else
             @foreach ($magazine as $evento)
-            <div class="container my-3">
-                <div class="row text-center p-1 gx-0 mb-1  shadow-sm  border rounded border-1">
-                    <div class="col  pr-0">
-                    
-                        <div class="h4 fw-light mb-0">18</div> 
-                        <div class="small text-muted">Jul</div>
+                <div class="container my-3">
+                    <div class="row text-center p-1 gx-0 mb-1  shadow-sm  border rounded border-1">
+                        <div class="col  pr-0">
                         
-                        <div class="round-circle">1</div> 
-                        
-                    </div>
+                            <div class="h4 fw-light mb-0">18</div> 
+                            <div class="small text-muted">Jul</div>
+                            
+                            <div class="round-circle">1</div> 
+                            
+                        </div>
 
-                    <div class="col-7  p-0">
-                    <div class="fs-md fw-normal text-start"><a class="text-dark" href="">
-                        {{ucwords(trans(Str::limit($evento->name, 24)))}}</a></div>
-                    <div class="text-muted fs-sm text-start">
-                        
-                    </div>  
-                    <div class="text-muted fs-sm text-start"></div>
-                    </div>
+                        <div class="col-7  p-0">
+                        <div class="fs-md fw-normal text-start"><a class="text-dark" href="">
+                            {{ucwords(trans(Str::limit($evento->name, 24)))}}</a></div>
+                        <div class="text-muted fs-sm text-start">
+                            
+                        </div>  
+                        <div class="text-muted fs-sm text-start"></div>
+                        </div>
 
-                    <div class="col-3  p-0">
-                        @if(is_null($evento->image))
-                            <a class="card-img-top d-block overflow-hidden" href="{{route('partner.magazine',['trackcustomer' => 'image-magazine'])}}">Add</a>
+                        <div class="col-3  p-0">
+                            @if(is_null($evento->image))
+                                <a class="card-img-top d-block overflow-hidden" href="{{route('partner.magazine',['trackcustomer' => 'image-magazine'])}}">Add</a>
+                                <a class="card-img-top d-block overflow-hidden" href="#" onclick="confirm('Are you sure, You want to delete this Entity?') || event.stopImmediatePropagation()"  wire:click.prevent="Magazinedelete({{$evento->id}})">  <i class="bi bi-x"></i> </a>
+                                @else
+                            <a class="card-img-top d-block overflow-hidden" href="">
+                            <img src="{{url('public/assets/image/exhibition/'.$evento->image)}}" alt="{{Str::limit($evento->name, 24)}}" href="{{route('admin.magazine',['slug' => $evento->slug, 'formm' => 'image' ])}}"></a>
                             <a class="card-img-top d-block overflow-hidden" href="#" onclick="confirm('Are you sure, You want to delete this Entity?') || event.stopImmediatePropagation()"  wire:click.prevent="Magazinedelete({{$evento->id}})">  <i class="bi bi-x"></i> </a>
-                            @else
-                        <a class="card-img-top d-block overflow-hidden" href="">
-                        <img src="{{url('public/assets/image/exhibition/'.$evento->image)}}" alt="{{Str::limit($evento->name, 24)}}" href="{{route('admin.magazine',['slug' => $evento->slug, 'formm' => 'image' ])}}"></a>
-                        <a class="card-img-top d-block overflow-hidden" href="#" onclick="confirm('Are you sure, You want to delete this Entity?') || event.stopImmediatePropagation()"  wire:click.prevent="Magazinedelete({{$evento->id}})">  <i class="bi bi-x"></i> </a>
-                        @endif
+                            @endif
+                        </div>
                     </div>
                 </div>
-            </div>
             @endforeach
         @endif
 
     @endif
 
-      @if($trackcustomer == 'add-magazine')     
+    @if($trackcustomer == 'add-magazine')     
+    <div class="container">
+        <small class="mt-3 mb-4 fw-bold">List your Business Magazine</small>
+        <form wire:submit.prevent="added">
+            <input type="text" class="form-control mb-1"  placeholder = "name"  wire:model.lazy="name" >
+            <input type="text" class="form-control mb-1"  placeholder = "type"  wire:model.lazy="type" >
+            <input type="text" class="form-control mb-1"  placeholder = "subscriber"  wire:model.lazy="subscriber" >
+            <textarea type="text" class="form-control mb-1"  placeholder = "desc" rows="3" wire:model.lazy="desc" > </textarea>
+            <input type="text" class="form-control mb-1"  placeholder = "frequency"  wire:model.lazy="frequency" >
+            <button class="btn btn-primary mt-2" type="submit">Submit</button>
+        </form>
+    </div>
+    @endif
+
+    @if($trackcustomer == 'image-magazine')
+    <div class="container">
+    <small class="mt-3 mb-4 fw-bold">Update Magazine Cover</small>
+        <form wire:submit.prevent = "image">
+        <input type="file" class="form-control mb-1"  placeholder = "image"  wire:model="image" >
+        <div class="btn btn-primary form-control mb-1">Submit</div>
+        </form>
+    </div>
+    @endif
+
+    @if($trackcustomer == 'print-facts')
         <div class="container">
-          <p>List your Business Magazine</p>
-            <form wire:submit.prevent="added">
-             <input type="text" class="form-control mb-1"  placeholder = "name"  wire:model.lazy="name" >
-             <input type="text" class="form-control mb-1"  placeholder = "type"  wire:model.lazy="type" >
-             <input type="text" class="form-control mb-1"  placeholder = "subscriber"  wire:model.lazy="subscriber" >
-             <textarea type="text" class="form-control mb-1"  placeholder = "desc" rows="3" wire:model.lazy="desc" > </textarea>
-             <input type="text" class="form-control mb-1"  placeholder = "frequency"  wire:model.lazy="frequency" >
-             <button class="btn btn-primary mt-2" type="submit">Submit</button>
+            <small class="mt-3 mb-4 fw-bold">Add Print Facts</small>
+            <form wire:submit.prevent="printedfacts">
+                <input type="text" class="form-control mb-1"  placeholder = "released"  wire:model.lazy="released" >
+                <input type="text" class="form-control mb-1"  placeholder = "updated"  wire:model.lazy="updated" >
+                <input type="text" class="form-control mb-1"  placeholder = "version" wire:model.lazy="version" >
+                <input type="text" class="form-control mb-1"  placeholder = "category"  wire:model.lazy="category" >
+                <button class="btn btn-primary mt-2" type="submit">Submit</button>
             </form>
         </div>
-      @endif
+    @endif
 
-      @if($trackcustomer == 'image-magazine')
+    @if($trackcustomer == 'contact')
         <div class="container">
-          <form wire:submit.prevent = "image">
-          <input type="file" class="form-control mb-1"  placeholder = "image"  wire:model="image" >
-            <div class="btn btn-primary form-control mb-1">Submit</div>
-          </form>
+        <small class="mt-3 mb-4 fw-bold">Add Business Contact</small>
+            <form wire:submit.prevent="addContact">
+                <input type="text" class="form-control mb-1" wire:model.lazy = "name" placeholder="name">
+                <input type="text" class="form-control mb-1" wire:model.lazy = "phone" placeholder="phone">
+                <input type="text" class="form-control mb-1" wire:model.lazy = "email" placeholder="email">
+                <input type="text" class="form-control mb-1" wire:model.lazy = "designation" placeholder="designation">
+                <button class="btn btn-primary form-control" type="submit">Submit</button>
+            </form>
         </div>
-      @endif
-
+    @endif
 
 
 
@@ -118,18 +144,6 @@
 
 
 
-        @if($trackcustomer == 'contact')
-
-
-
-        @endif
-
-        //choose industry
-            <form action="">
-                <input type="text" class="form-control" wire:model.lazy = "industry">
-                <button class="btn btn-primary form-control" type="submit">Submit</button>
-            </form>
-
             <form wire:submit.prevent="addMagazine">
                 <input type="text" class="form-control" wire:model.lazy = "magazineName" placeholder="Magazine Name">
                 <input type="text" class="form-control" wire:model.lazy = "brandName" placeholder="Brand">
@@ -142,31 +156,21 @@
                 <button class="btn btn-primary form-control" type="submit">Submit</button>
             </form>
 
-            <form wire:submit.prevent="addContact">
-                <input type="text" class="form-control" wire:model.lazy = "name" placeholder="Owner Name">
-                <input type="text" class="form-control" wire:model.lazy = "phone" placeholder="phone">
-                <input type="text" class="form-control" wire:model.lazy = "email" placeholder="email">
-                <button class="btn btn-primary form-control" type="submit">Submit</button>
-            </form>
 
+    @if($trackcustomer == 'event')
+                //choose or select 
+                <!-- <div class="small">Distribution Audience</div>
+                    <form wire:submit.prevent="businessAudience">
+                            <input type="text" wire:model.lazy = "email" placeholder="email">
+                    </form> -->
 
-        <div class="small">Upload Magazine Images</div>
+                //choose or select
+                <div class="small">Upcoming Exhibition, where you are going to participate? May be/ May be Not?</div>
+                <form wire:submit.prevent="chooseEvent">
 
-        <form wire:submit.prevent="multiImage">
+                </form>
 
-        </form>
-
-        //choose or select 
-        <!-- <div class="small">Distribution Audience</div>
-            <form wire:submit.prevent="businessAudience">
-                    <input type="text" wire:model.lazy = "email" placeholder="email">
-            </form> -->
-
-        //choose or select
-        <div class="small">Upcoming Exhibition, where you are going to participate? May be/ May be Not?</div>
-        <form wire:submit.prevent="chooseEvent">
-
-        </form>
+    @endif
 
             <div class="small">When going to print?</div>
             <form wire:submit.prevent="date">
@@ -204,27 +208,73 @@
             </form>
 
 
-        <div class="small">Upload Magazine Images</div>
+            <div class="small">Upload Magazine Images</div>
 
-        <form wire:submit.prevent="multiImage">
+            <form wire:submit.prevent="multiImage">
 
-        </form>
-
-        //choose or select 
-        <!-- <div class="small">Distribution Audience</div>
-            <form wire:submit.prevent="businessAudience">
-                    <input type="text" wire:model.lazy = "email" placeholder="email">
-            </form> -->
-
-        //choose or select
-        <div class="small">Upcoming Exhibition, where you are going to participate? May be/ May be Not?</div>
-        <form wire:submit.prevent="chooseEvent">
-
-        </form>
-
-            <div class="small">When going to print?</div>
-            <form wire:submit.prevent="date">
-                <input type="text" class="form-control" wire:model.lazy = "date" placeholder="Print Date">
             </form>
-    @endif
+
+            //choose or select 
+            <!-- <div class="small">Distribution Audience</div>
+                <form wire:submit.prevent="businessAudience">
+                        <input type="text" wire:model.lazy = "email" placeholder="email">
+                </form> -->
+
+            //choose or select
+            <div class="small">Upcoming Exhibition, where you are going to participate? May be/ May be Not?</div>
+            <form wire:submit.prevent="chooseEvent">
+
+            </form>
+
+                <div class="small">When going to print?</div>
+                <form wire:submit.prevent="date">
+                    <input type="text" class="form-control" wire:model.lazy = "date" placeholder="Print Date">
+                </form>
+        @endif
+
+
+
+
+
+    <div class="handheld-toolbar">
+      <div class="d-table table-layout-fixed w-100">
+      
+
+          <a class="d-table-cell handheld-toolbar-item {{'admin/dashboard/blog' == request()->path() ? 'active' : '' }}" href="{{route('partner.magazine',['trackcustomer' => 'print-facts'])}}">
+            <span class="handheld-toolbar-icon">
+            <i class="ci-filter-alt"></i></span>
+            <span class="handheld-toolbar-label {{'admin/dashboard/blog' == request()->path() ? 'active' : '' }}">Facts</span>
+          </a>
+          
+          <a class="d-table-cell handheld-toolbar-item" href="{{route('partner.magazine',['trackcustomer' => 'contact'])}}">
+            <span class="handheld-toolbar-icon"><i class="ci-cart"></i></span>
+            <span class="handheld-toolbar-label">Contact</span>
+          </a>
+
+      
+          <a class="d-table-cell handheld-toolbar-item {{'admin/dashboard/event' == request()->path() ? 'active' : '' }}" href="{{route('partner.magazine',['trackcustomer' => 'inquiry'])}}">
+            <span class="handheld-toolbar-icon">
+            <i class="ci-filter-alt"></i></span>
+            <span class="handheld-toolbar-label {{'admin/dashboard/event' == request()->path() ? 'active' : '' }}">Inquiry</span>
+          </a>
+
+          <a class="d-table-cell handheld-toolbar-item {{'admin/dashboard/event' == request()->path() ? 'active' : '' }}" href="{{route('partner.magazine',['trackcustomer' => 'event'])}}">
+            <span class="handheld-toolbar-icon">
+            <i class="ci-filter-alt"></i></span>
+            <span class="handheld-toolbar-label {{'admin/dashboard/event' == request()->path() ? 'active' : '' }}">Event</span>
+          </a>
+          
+          <a class="d-table-cell handheld-toolbar-item" href="{{route('partner.magazine',['trackcustomer' => 'business'])}}">
+            <span class="handheld-toolbar-icon"><i class="bi bi-add"></i></span>
+            <span class="handheld-toolbar-label">Plan</span>
+          </a>
+      
+
+        <a class="d-table-cell handheld-toolbar-item" data-bs-toggle="offcanvas" href="#offcanvasExample" role="button" aria-controls="offcanvasExample">
+            <span class="handheld-toolbar-icon"><i class="ci-heart"></i></span>
+            <span class="handheld-toolbar-label">Menu</span>
+        </a>
+
+      </div>
+    </div>
 </main>
