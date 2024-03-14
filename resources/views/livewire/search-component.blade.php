83ca995f6a3d4f7fa05943f743559a09da590468
lara <laravel8coi@gmail.com>
2024-03-14 20:56:30 +0530

    14march20240856

--- resources/views/livewire/search-component.blade.php
+++ resources/views/livewire/search-component.blade.php
@@ -59,102 +59,10 @@
             <div class="container">
                 <div class="row row-cols-2 row-cols-lg-6 gy-2 gx-3 g-lg-3">
                     <div class="col">
-                        <a  href="{{route('admin.dashboard', ['board' => 'event'])}}">
-                           <div class="p-3 border rounded border-dark bg-light text-center">  Event</div>
+                        <a  href="#">
+                           <div class="p-3 border rounded border-dark bg-light text-center">category</div>
                         </a> 
                     </div>
-                    <div class="col">
-                         <a href="{{route('admin.dashboard', ['board' => 'order'])}}">
-                            <div class="p-3 border rounded border-dark bg-light text-center">Order</div>
-                        </a> 
-                    </div>
-                    <div class="col">
-                         <a href="{{route('admin.dashboard', ['board' => 'job'])}}">
-                            <div class="p-3 border rounded border-dark bg-light text-center">Job</div>
-                        </a> 
-                    </div>
-                   
-                    <div class="col">
-                         <a href="{{route('admin.dashboard', ['board' => 'client'])}}">
-                            <div class="p-3 border rounded border-dark bg-light text-center">Visitor</div>
-                        </a> 
-                    </div>
-                    
-                    <div class="col">
-                         <a href="{{route('admin.dashboard', ['board' => 'blog'])}}">
-                            <div class="p-3 border rounded border-dark bg-light text-center">Blog</div>
-                        </a> 
-                    </div>
-
-                    <div class="col">
-                        <a  href="{{route('all.category')}}">
-                           <div class="p-3 border rounded border-dark bg-light text-center">Category</div>
-                        </a> 
-                    </div>
-
-                    <div class="col">
-                        <a  href="{{route('admin.dashboard', ['board' => 'createhashtagss'])}}">
-                           <div class="p-3 border rounded border-dark bg-light text-center">Hashtag</div>
-                        </a> 
-                    </div>
-
-
-                    <div class="col">
-                        <a  href="{{route('admin.dashboard', ['board' => 'magazine'])}}">
-                           <div class="p-3 border rounded border-dark bg-light text-center">Magazine</div>
-                        </a> 
-                    </div>
-
-                    <div class="col">
-                        <a  href="{{route('admin.dashboard', ['board' => 'review'])}}">
-                           <div class="p-3 border rounded border-dark bg-light text-center">Review</div>
-                        </a> 
-                    </div>
-
-                    <div class="col">
-                        <a  href="{{route('admin.dashboard', ['board' => 'visitor'])}}">
-                           <div class="p-3 border rounded border-dark bg-light text-center">User</div>
-                        </a> 
-                    </div>
-
-                    <div class="col">
-                        <a  href="{{route('admin.dashboard', ['board' => 'viewso'])}}">
-                           <div class="p-3 border rounded border-dark bg-light text-center">Views</div>
-                        </a> 
-                    </div>
-
-                    <div class="col">
-                        <a  href="{{route('admin.dashboard', ['board' => 'bulkReview'])}}">
-                           <div class="p-3 border rounded border-dark bg-light text-center">Reviews</div>
-                        </a> 
-                    </div>
-
-                    <div class="col">
-                        <a  href="{{route('admin.dashboard', ['board' => 'organizer'])}}">
-                           <div class="p-3 border rounded border-dark bg-light text-center">Organiser</div>
-                        </a> 
-                    </div>
-                    <div class="col">
-                        <a href="{{route('admin.dashboard', ['board' => 'new-organiser'])}}">
-                           <div class="p-3 border rounded border-dark bg-light text-center">Add Organiser</div>
-                        </a> 
-                    </div>
-
-                    
-
-                    <div class="col">
-                        <a  href="{{route('admin.questionadd')}}">
-                           <div class="p-3 border rounded border-dark bg-light text-center">Question</div>
-                        </a> 
-                    </div>
-
-                    <div class="col">
-                        <a  href="{{route('admin.dashboard', ['board' => 'ticketPlan'])}}">
-                           <div class="p-3 border rounded border-dark bg-light text-center">Plan</div>
-                        </a> 
-                    </div>
-
-                    
                 </div>
             </div>
           </div>
